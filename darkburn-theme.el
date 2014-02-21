95356fd3c48d2a9f69e322fe70ea7d75ecef749b
Jonas Gorauskas <jgorauskas@gmail.com>
2014-02-21 00:12:02 -0800

    format header properly for MELPA

--- darkburn-theme.el
+++ darkburn-theme.el
@@ -1,7 +1,6 @@
 ;;; darkburn-theme.el --- A not-so-low contrast color theme for Emacs.
-;; Modified: 2014-02-20 16:59:58
 
-;; Copyright (C) 2014 Jonas Gorauskas
+;; Copyright 2014 Jonas Gorauskas
 
 ;; Author: Jonas Gorauskas <jgorauskas@gmail.com>
 ;; URL: http://github.com/gorauskas/darkburn-theme
@@ -28,8 +27,7 @@
 
 ;;; Credits:
 
-;; Bozhidar Batsov deserves all the credit!
-;; I only changed a few colors.
+;; Bozhidar deserves all the credit! I only changed a few colors.
 
 ;;; Code:
 
